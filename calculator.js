7a0c02b9a443f5ec5b9c98a9c4aa2ba32f040ba8
Rohail-Tauq <mrohail9932@gmail.com>
2024-09-07 18:02:54 +0500

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,38 +1,3 @@
-// const display=document.querySelector("#show")
-// let currentinput ='';
-// function updatedisplay(){
-//     display.innerText=currentinput;
-// }
-// function handleclick(value){
-// if(value=='C'){
-//     currentinput='';
-// }
-// else if(value==="="){
-//     currentinput=eval(currentinput);
-// }
-// else{
-//         currentinput += value;
-// }
-// updatedisplay();
-// }
-//     document.getElementById('btn0').addEventListener('click', () => handleclick('0'));
-//     document.getElementById('btn1').addEventListener('click', () => handleclick('1'));
-//     document.getElementById('btn2').addEventListener('click', () => handleclick('2'));
-//     document.getElementById('btn3').addEventListener('click', () => handleclick('3'));
-//     document.getElementById('btn4').addEventListener('click', () => handleclick('4'));
-//     document.getElementById('btn5').addEventListener('click', () => handleclick('5'));
-//     document.getElementById('btn6').addEventListener('click', () => handleclick('6'));
-//     document.getElementById('btn7').addEventListener('click', () => handleclick('7'));
-//     document.getElementById('btn8').addEventListener('click', () => handleclick('8'));
-//     document.getElementById('btn9').addEventListener('click', () => handleclick('9'));
-//     document.getElementById('addition').addEventListener('click', () => handleclick('+'));
-//     document.getElementById('minus').addEventListener('click', () => handleclick('-'));
-//     document.getElementById('divi').addEventListener('click', () => handleclick('/'));
-//     document.getElementById('star').addEventListener('click', () =>handleclick('*'));
-//     document.getElementById('point').addEventListener('click', () => handleclick('.'));
-//     document.getElementById('C').addEventListener('click', () => handleclick('C'));
-//     document.getElementById('equal').addEventListener('click', () => handleclick('='));
-
 const display=document.querySelector("#show");
 let currentinput='';
 function updateDisplay() {
